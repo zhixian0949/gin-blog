9279f3bcf370074b38c94db5c715ed3bf979f553
zhixian <1406276078@qq.com>
2021-07-18 01:12:59 +0800

    middleware

---
+++ global/setting.go
@@ -0,0 +1,9 @@
+package global
+
+import "github.com/zhixian0949/gin-blog/pkg/setting"
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettingS
+)
--- main.go
+++ main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/zhixian0949/gin-blog/global"
 	"github.com/zhixian0949/gin-blog/internal/routers"
+	"github.com/zhixian0949/gin-blog/pkg/setting"
 )
 
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+}
+
 func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
 	router := routers.NewRouter()
 	s := &http.Server{
-		Addr:           ":8080",
+		Addr:           ":" + global.ServerSetting.HttpPort,
 		Handler:        router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
 		MaxHeaderBytes: 1 << 20,
 	}
+	fmt.Printf("%+v\n", global.AppSetting)
+	fmt.Printf("%+v\n", global.DatabaseSetting)
+	fmt.Printf("%+v\n", global.ServerSetting)
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
 }
