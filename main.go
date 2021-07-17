43a8b675d09ae606e19b2dcd977efa3c856457d6
zhixian <1406276078@qq.com>
2021-07-18 00:48:41 +0800

    init

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/zhixian0949/gin-blog/internal/routers"
+)
+
+func main() {
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
+}
